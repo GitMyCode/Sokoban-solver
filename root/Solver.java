a14318fd287c79c68fa4c59fb9faae6e7922692e
Martin Bouchard <marbouchl@gmail.com>
2015-01-22 17:44:02 -0500

    Update Solver.java
    Remettre l'entré du main pour prendre les commandes en console

--- root/Solver.java
+++ root/Solver.java
@@ -17,8 +17,8 @@ import sokoban.Probleme;
 public class Solver {
     
     public static void main(String args[]) throws IOException {
-        //Reader reader = args.length==1 ? new FileReader(args[0]) : new InputStreamReader(System.in);
-        Reader reader =  new FileReader("tests/sokoban14.txt");
+        Reader reader = args.length==1 ? new FileReader(args[0]) : new InputStreamReader(System.in);
+        //Reader reader =  new FileReader("tests/sokoban14.txt");
         BufferedReader br = new BufferedReader(reader);
         Probleme probleme = Probleme.charger(br);
         List<astar.Action> plan = astar.AStar.genererPlan(probleme.grille, probleme.etatInitial, probleme.but, probleme.but);
